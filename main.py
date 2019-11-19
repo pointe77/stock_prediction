61cc248875609d0201667340a2d4fe9cd23a2c1d
Ryan Lee <strike77@gmail.com>
2019-11-18 22:47:26 -0800

    Initial upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
+
+file_to_save = 'resources/AAL.csv'
+df = pd.read_csv(file_to_save)
+df = df.sort_values('Date')
+
+# Double check the result
+print(df.head())
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
